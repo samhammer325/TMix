294c52b713c321bae3d82f3c11006a586b2fa1fb
Glenn Sullivan <sullivangln87@gmail.com>
2016-01-20 17:08:16 -0700

    fix user model typo and work on user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :rating, dependent: :destroy
+  has_many :ratings, dependent: :destroy
   has_many :playlists, through: :ratings, dependent: :destroy
   has_many :songs, dependent: :destroy
 
