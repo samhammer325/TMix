84af43ba17c25000bcaf33dd63ae7a73487662ec
Noah Loveland <lovelandnoah@gmail.com>
2016-01-20 20:11:14 -0700

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   protect_from_forgery with: :exception
+  def ensure_signup_complete
+  	return if action_name == "finish_signup"
 
-  
+  	if current_user && !current_user.email_verified?
+  		redirect_to finish_signup_path(current_user)
+  	end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
