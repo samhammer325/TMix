68545949ea9cc3339d4389b91cc4ec980f6afa65
Noah Loveland <lovelandnoah@gmail.com>
2016-01-21 10:40:29 -0700

    twitter and facebook omniauth is working together

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"]
-  config.omniauth :twitter, 'KEY', "SECRET"
+  config.omniauth :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_SECRET"]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
