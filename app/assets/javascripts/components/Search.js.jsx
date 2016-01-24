0c88dbc527850c12f3a61e456eadfe92f9cf4198
Sam Hammer <sam@michaelrobertson.com>
2016-01-23 23:35:57 -0700

    can search for artists

--- app/assets/javascripts/components/Search.js.jsx
+++ app/assets/javascripts/components/Search.js.jsx
@@ -1,37 +1,38 @@
 class Search extends React.Component{
   constructor(props){
     super(props);
-    // this.search = this.search.bind(this)
     this.state = {results: []};
+    this.getSearchResults = this.getSearchResults.bind(this);
+    this.getStationId = this.getStationId.bind(this);
   }
-
   componentDidMount(){
-    // this.getTweets()
   }
 
-  getSearcheResults(){
-    $.ajax({
-      // url: "http://api.dar.fm/playlist.php?callback=json&partner_token=9388418650&q=@artist%black",
-      url: "http://api.dar.fm/playlist.php?&q=@artist%black&callback=?&partner_token=9388418650",
-      type: 'GET',
-      dataType: 'jsonp'
-      // debugger
-      // data: { artist: this.refs.searchText.value }
-    }).success( data => {artist: artist}
-      debugger
-      // this.setState({tweets: data})
-    })
+  getStationId(){
+    debugger
+  }
 
+  getSearchResults(){
+    self = this;
+    let searchTerm = self.refs.searchText.value.replace(/\s/g, "%20")
+    $.ajax({
+      url: "http://api.dar.fm/playlist.php?&q=@artist%" + searchTerm + "&callback=jsonp&partner_token=9388418650",
+      jsonp: 'callback',
+      //type: 'GET',
+      dataType: 'jsonp',
+    }).success( data => {
+
+      this.setState({artist: data});
+      this.getStationId();
+    });
   }
 
   render(){
-
     return(
         <div>
           <h5>Search for an artist:</h5>
           <input type='text' ref='searchText' placeholder="Artist"/>
-          <button onClick={this.getSearcheResults} class='btn'>Search</button>
-
+          <span onClick={this.getSearchResults} className='btn'>Search</span>
         </div>)
   }
 }
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @search_term = params[:artist]
+  end
+
+  def getSearchResults
+    # render 'index'
   end
 end
