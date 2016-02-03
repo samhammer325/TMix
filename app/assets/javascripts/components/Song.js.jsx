c66afc6c0510802ecc8e937ab448647f320d1a8e
Spencer Christopherson <schristopherson@gmail.com>
2016-02-02 21:17:13 -0700

    play mixtapes

--- app/assets/javascripts/components/Song.js.jsx
+++ app/assets/javascripts/components/Song.js.jsx
@@ -1,7 +1,7 @@
 class Song extends React.Component{
   constructor(props){
     super(props);
-    this.state = {results: [], streaming: false, station: 0};
+    this.state = {results: [], streaming: false, station_id: 0};
     this.renderPlayButton = this.renderPlayButton.bind(this);
     // this.addButton = this.addButton.bind(this);
     this.play = this.play.bind(this);
@@ -11,8 +11,10 @@ class Song extends React.Component{
         // debugger
   }
   componentDidMount(){
-    this.startTimer();
-     
+    //this.startTimer();
+
+    // Calls function multiple times on a repeating interval
+    setInterval(function(){ this.checkIfStreaming(); }, 10000);
 
   }
   
@@ -52,10 +54,11 @@ class Song extends React.Component{
       this.setState({results: data});
       // debugger
       if (this.state.results.length != 0) {
-        debugger
         // this.state.station = this.state.results[0].station_id;
-        this.setState({station_id: this.state.results[0].station_id});
+        // this.setState({station_id: this.state.results[0].station_id});
+        this.setState({station_id: data[0].station_id});
         this.setState({streaming: true});
+
       };
 
 
@@ -66,7 +69,7 @@ class Song extends React.Component{
     let player = document.getElementById("player")
     // http://api.dar.fm/player_api.php?station_id=6480&custom_style=radioslice&partner_token=9388418650
     // player.src = "https://apidarfm.global.ssl.fastly.net/player_api.php?station_id=" + station + "&custom_style=radioslice&partner_token=9388418650"
-    player.src = "http://api.dar.fm/player_api.php?station_id=" + station + "&custom_style=radioslice&partner_token=9388418650"
+    player.src = "http://api.dar.fm/player_api.php?station_id=" + this.state.station_id + "&custom_style=radioslice&partner_token=9388418650"
   }
 
   // addButton(){
@@ -85,7 +88,7 @@ class Song extends React.Component{
 
   render(){
     // debugger
-    // this.renderPlayButton;
+    this.renderPlayButton();
 
         if (this.state.streaming == true) {
           // alert('streaming');
